5bc35840b6fb4cc5c6c010d118014d39b2920413
Абдурахман Усманов <ausmanov706@gmail.com>
2024-04-11 00:40:21 +0300

    feat: created movie card

---
+++ src/components/MovieCard/MovieCard.tsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+import { Movie } from "../../types/Movie";
+import loadErrorImage from "../../assets/load-error.jpg";
+
+import s from "./MovieCard.module.css";
+
+const MovieCard = (props: Movie) => {
+  const { id, backdrop, name, shortDescription, rating, year, genres } = props;
+  const raitingStyles = rating.imdb >= 8 ? s.great : rating.imdb >= 5 ? s.good : s.bad;
+
+  return (
+    <div className={s.wrapper}>
+      <span className={`${s.raiting} ${raitingStyles}`}>{rating.imdb}</span>
+      <div className={s.poster_wrapper}>
+        <Link to={`movie/${id}`}>
+          <img 
+            src={backdrop.previewUrl} 
+            alt="movie card" 
+            className={s.poster_img} 
+            onError={({currentTarget}) => {
+              currentTarget.onerror = null;
+              currentTarget.src = loadErrorImage;
+            }} 
+          />
+        </Link>
+      </div>
+      <h3 className={s.title}>
+        <Link to={`movie/${id}`}>
+          {name}
+        </Link>
+      </h3>
+      <div className={s.short_description}>
+        {shortDescription}
+      </div>
+      <div className={s.info}>
+        <span>{year}</span>
+        <div className={s.genres}>
+          {genres.map((genre, i) => <span key={i}>{genre.name}</span>)}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,27 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { fetchMovies } from "../../store/slices/movieSlice";
+
 import Wrapper from "../../components/Wrapper/Wrapper";
+import MovieCard from "../../components/MovieCard/MovieCard";
 
 const HomePage = () => {
+  const dispatch = useAppDispatch();
+  const movies = useAppSelector((state) => state.movie.movies);
+
+  useEffect(() => {
+    dispatch(fetchMovies({
+      currentPage: 2
+    }))
+  }, [dispatch])
+
   return (
     <>
-      <Wrapper>home</Wrapper>
+      <Wrapper>
+        {movies.map((movie) => (
+          <MovieCard {...movie} />
+        ))}
+      </Wrapper>
     </>
   );
 };
