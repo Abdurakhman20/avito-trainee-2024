5891f0ee719a2104f5355d5e8f1df36d311dd7f5
Абдурахман Усманов <ausmanov706@gmail.com>
2024-04-10 18:33:20 +0300

    fix: hotfix

--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -1,8 +1,24 @@
+
+type Person = {
+  id: number;
+  photo: string;
+  name: string;
+  enName: string;
+  description: string;
+  profession: string;
+  enProfession: string;
+}
+type Country = {
+  name: string;
+}
+type Genre = {
+  name: string;
+}
+
 export type Movie = {
   id: number;
   name: string;
   ageRating: number;
-  alternativeName: string;
   description: string;
   shortDescription: string;
   logo: {
@@ -12,12 +28,9 @@ export type Movie = {
     previewUrl: string;
     url: string;
   };
-  genres: {
-    name: string;
-  }[];
-  countries: {
-    name: string;
-  }[];
+  genres: Genre[];
+  countries: Country[];
+  persons: Person[];
   rating: {
     filmCritics: number;
     imdb: number;
